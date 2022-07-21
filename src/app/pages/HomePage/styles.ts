ef6127581c8072f732c8dfa6f64f61bbe1b95a3e
James Nguyen <nguyenthecoder@gmail.com>
2022-07-21 12:03:28 -0400

    added service in package.j

--- src/app/pages/HomePage/styles.ts
+++ src/app/pages/HomePage/styles.ts
@@ -1,17 +1,4 @@
 const styles = {
-  listContainer: {
-    display: 'flex',
-    flexDirection: 'column',
-    alignItems: 'center',
-    marginTop: 1,
-    marginBottom: 3,
-    // overflowX: 'hidden',
-    // overflowY: 'auto',
-    // height: 500,
-    // maxHeight: '100%',
-    // height: '50%',
-    // flex: 1,
-  },
 };
 
 export { styles };
